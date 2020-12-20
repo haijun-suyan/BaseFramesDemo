b9c0d2147c56c0fc58cd9364fd7f11fa6d1b884b
yt_yanhaijun <C2019275@pccc.bankcomm.com>
2020-12-20 14:32:36 +0800

    项目基本框架初始

---
+++ BaseFramesDemo/BaseFramesDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  BaseFramesDemo
+//
+//  Created by haijunyan on 2020/12/20.
+//  Copyright © 2020 haijunyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (nonatomic,strong) UIWindow *window;
+@end
+
---
+++ BaseFramesDemo/BaseFramesDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  BaseFramesDemo
+//
+//  Created by haijunyan on 2020/12/20.
+//  Copyright © 2020 haijunyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
