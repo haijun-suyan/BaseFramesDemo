34d54f2e5cc8346948ba1e80df91cfd0771454ae
yt_yanhaijun <C2019275@pccc.bankcomm.com>
2020-12-29 22:52:38 +0800

    批量引入文件(夹)

---
+++ BaseFramesDemo/BaseFramesDemo/BatchIntroduceAPI/BatchIntroduceBase.h
@@ -0,0 +1,15 @@
+//  批量引入文件(基础文件(存放通用性引入文件))
+//  BatchIntroduceBase.h
+//  BaseFramesDemo
+//
+//  Created by haijunyan on 2020/12/29.
+//  Copyright © 2020 haijunyan. All rights reserved.
+//
+
+#ifndef BatchIntroduceBase_h
+#define BatchIntroduceBase_h
+
+//#import "CustomViewController.h"
+
+
+#endif /* BatchIntroduceBase_h */
---
+++ BaseFramesDemo/BaseFramesDemo/BatchIntroduceAPI/BatchIntroduceScene.h
@@ -0,0 +1,13 @@
+//  批量引入文件(特定场景补充)
+//  BatchIntroduceScene.h
+//  BaseFramesDemo
+//
+//  Created by haijunyan on 2020/12/29.
+//  Copyright © 2020 haijunyan. All rights reserved.
+//
+
+#ifndef BatchIntroduceScene_h
+#define BatchIntroduceScene_h
+#import "BatchIntroduceBase.h"
+
+#endif /* BatchIntroduceScene_h */
