3c8dd81e258350189d2cfd0295626deef36f845a
yt_yanhaijun <C2019275@pccc.bankcomm.com>
2021-01-04 15:35:35 +0800

    编写某些全局型涉及配置信息的参数全局变量名(第三方服务商appId或Appkey设置)

---
+++ BaseFramesDemo/BaseFramesDemo/Config/PCConfig.h
@@ -0,0 +1,13 @@
+//
+//  PCConfig.h
+//  BaseFramesDemo
+//
+//  Created by haijunyan on 2021/1/4.
+//  Copyright © 2021 haijunyan. All rights reserved.
+//
+
+#ifndef PCConfig_h
+#define PCConfig_h
+
+
+#endif /* PCConfig_h */
---
+++ BaseFramesDemo/BaseFramesDemo/Config/ReadMe.h
@@ -0,0 +1,21 @@
+//
+//  ReadMe.h
+//  BaseFramesDemo
+//
+//  Created by haijunyan on 2021/1/4.
+//  Copyright © 2021 haijunyan. All rights reserved.
+//
+
+#ifndef ReadMe_h
+#define ReadMe_h
+/*
+ 4.0.0
+ 版本功能概略描述
+
+
+
+ //SVN版本忽略文件
+ *.DS_Store,build/,*.pbxuser,!default.pbxuser,*.mode1v3,!default.mode1v3,*.mode2v3,!default.,mode2v3,*.perspectivev3,!default.perspectivev3,!default.xcworkspace,xcuserdata,profile,*.moved-aside,DerivedData,.idea/,*.xcscmblueprint,*.xcshareddata,*.xcuserdatad,*.xcscmblueprint,Pods,Podfile.lock,.xcuserdatad,.xcscmblueprint,xcuserdata,*.xcuserstate,.xcuserstate,.Pods,.git,.pbxproj,project.pbxproj,*.o,*.lo,*.la,*.al,.libs,*.so,*.so.[0-9]*,*.pyc,*.pyo,*.rej,*~,#*#,.#*,.*.swp,.DS_Store,.xcuserstate
+*/
+
+#endif /* ReadMe_h */
