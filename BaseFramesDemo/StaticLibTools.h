d371b34a10b174c8cfa78ff378bde23ede00de22
yanhaijun <15261882407@163.com>
2023-10-18 00:53:02 +0800

    Type:ReqDev Desc:导入静态库libStaticLibTools.a文件和.h头文件 Version:

---
+++ BaseFramesDemo/StaticLibTools.h
@@ -0,0 +1,11 @@
+//
+//  StaticLibTools.h
+//  StaticLibTools
+//  声明(公开引线)
+//  Created by haijunyan on 2023/10/17.
+//
+
+#import <UIKit/UIKit.h>
+@interface StaticLibTools : NSObject
++ (void)testDemo;
+@end
